c4376196d144c60c125153f2abcdec88ca62c9fd
ayoublaroussi <ayoublaroussipro@gmail.com>
2024-11-23 19:13:13 +0100

    feat(src): update of procedure-stockee.sql

--- src/sql/procedure-stockee.sql
+++ src/sql/procedure-stockee.sql
@@ -1,26 +1,33 @@
 
 -- Création de la procédure stockée "Lister tous les objets situés dans une pièce passée en paramètre" 
 
-CREATE OR REPLACE FUNCTION lister_objet(nom_salle VARCHAR)
- RETURNS TABLE(nom_objets VARCHAR)
+CREATE OR REPLACE FUNCTION lister_objet(salle VARCHAR)
+ RETURNS TABLE(nom_objet VARCHAR)
  LANGUAGE sql
  AS $$
-    SELECT nom_objets
-    FROM objets
-    INNER JOIN salles ON salles.id_salles = objets.id_salles
-    WHERE nom_salles = nom_salle;
+    SELECT nom_objet
+    FROM objet
+    INNER JOIN salle ON salle.id_salle = objet.id_salle
+    WHERE salle = nom_salle;
  $$;
 
-/* Création de la procédure stockée "Ajout d'un objet passé en paramètre et association avec la pièce concernée en ID" */
+-- Pour l'appeler
+CALL lister_objet();
+
+
+-- Création de la procédure stockée "Ajout d'un objet passé en paramètre et association avec la pièce concernée en ID"
+
 CREATE OR REPLACE PROCEDURE ajout_objet_id_salle(nom_objets VARCHAR, id_salles INTEGER)
  LANGUAGE plpgsql
  AS $$
  BEGIN
-     INSERT INTO objets (nom_objets, id_salles) VALUES (nom_objets, id_salles);
+     INSERT INTO objets (nom_objets, id_salles) VALUES 
+        (nom_objets, id_salles);
  END;
  $$;
 
-/* Création de la procédure stockée "Ajout d'un objet passé en paramètre et association avec la pièce concernée avec le nom" */
+-- Création de la procédure stockée "Ajout d'un objet passé en paramètre et association avec la pièce concernée avec le nom"
+
  CREATE OR REPLACE PROCEDURE ajout_objet(var_nom_objets VARCHAR, var_nom_salles VARCHAR)
  LANGUAGE plpgsql
  AS $$
