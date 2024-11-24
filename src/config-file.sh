752e3f0957ae455bbe6b9a5e3255a2b85344f2c6
ayoublaroussi <ayoublaroussipro@gmail.com>
2024-11-24 15:26:01 +0100

    feat(src): update the config file .sh

--- src/config-file.sh
+++ src/config-file.sh
@@ -7,6 +7,5 @@ DB_USER="ayoublaroussi"
 psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/create.sql postgres
 psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/insert.sql "$DB_NAME"
 psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/trigger.sql "$DB_NAME"
-psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/procedure-stockee.sql "$DB_NAME"
 
 pgcli -u admin_simpluedo -d simpluedo
