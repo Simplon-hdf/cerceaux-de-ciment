80ba465b90097b496f44f838bdce697fe0c394a7
ayoublaroussi <ayoublaroussipro@gmail.com>
2024-11-24 14:57:18 +0100

    feat(src): update the config file .sh

--- src/config-file.sh
+++ src/config-file.sh
@@ -3,11 +3,10 @@
 # Variables de connexion PostgreSQL
 DB_NAME="simpluedo"
 DB_USER="ayoublaroussi"
-DB_HOST="localhost"
-DB_PORT="5432"
 
-psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/create.sql postgres
-psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/insert.sql simpluedo
-psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/trigger.sql simpluedo
+psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/create.sql postgres
+psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/insert.sql "$DB_NAME"
+psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/trigger.sql "$DB_NAME"
+psql -U "$DB_USER" -f /Users/ayoublaroussi/Documents/Repo/Simplon/simpluedo-cerceaux-de-ciment/src/sql/procedure-stockee.sql "$DB_NAME"
 
 pgcli -u admin_simpluedo -d simpluedo
